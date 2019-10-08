8bfefa9dd72347b598dca3067ccc81335159dcaf
Joe <1712833832@qq.com>
2019-10-08 15:03:59 +0800

    Add files via upload

---
+++ precache-manifest.abb58d83279149402cf1a78984bbb3fb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d73858b7f36c1529916",
+    "url": "/car/js/chunk-8f11a9aa.05bae3c9.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/car/robots.txt"
+  },
+  {
+    "revision": "85e275d75b988b2bc89d",
+    "url": "/car/css/chunk-a2750e12.4aa02e16.css"
+  },
+  {
+    "revision": "49af96fe5ce1cba5396e",
+    "url": "/car/css/chunk-0f4e08aa.c58c31d5.css"
+  },
+  {
+    "revision": "3d7aa14258b221059e56",
+    "url": "/car/css/chunk-76fc8942.ea8e6524.css"
+  },
+  {
+    "revision": "3d7aa14258b221059e56",
+    "url": "/car/js/chunk-76fc8942.ef4d944a.js"
+  },
+  {
+    "revision": "5d73858b7f36c1529916",
+    "url": "/car/css/chunk-8f11a9aa.9198486b.css"
+  },
+  {
+    "revision": "20b413012655f90073fe",
+    "url": "/car/js/app.c5bb8ef4.js"
+  },
+  {
+    "revision": "49af96fe5ce1cba5396e",
+    "url": "/car/js/chunk-0f4e08aa.c28dffa3.js"
+  },
+  {
+    "revision": "85e275d75b988b2bc89d",
+    "url": "/car/js/chunk-a2750e12.07194576.js"
+  },
+  {
+    "revision": "63e52254ff138e6ec307",
+    "url": "/car/js/chunk-vendors.46ee0f61.js"
+  },
+  {
+    "revision": "46283fb8d9aad382eec1184d1be93ca7",
+    "url": "/car/index.html"
+  },
+  {
+    "revision": "b2d1423dd78c3c511e155e4f7479db0b",
+    "url": "/car/css/reset.css"
+  },
+  {
+    "revision": "20b413012655f90073fe",
+    "url": "/car/css/app.12cf8a60.css"
+  }
+];
